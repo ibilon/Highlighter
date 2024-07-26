675d07ebc82e11cf65c7641ee9f0808bd95366bc
Tobiasz Laskowski <tobil4sk@outlook.com>
2024-07-27 00:49:09 +0100

    Update css generation

--- highlighter/CSS.hx
+++ highlighter/CSS.hx
@@ -12,7 +12,7 @@ class CSS
 		return style.join("\n");
 	}
 
-	// From https://github.com/Microsoft/vscode/blob/bdad49679a74feeedde2b1f5e3634d6b7aad0425/src/vs/editor/common/modes/supports/tokenization.ts#L386 (MIT License)
+	// From https://github.com/microsoft/vscode/blob/70ffbbae97485ba914dd4a7cd19f9f7d8af125cd/src/vs/editor/common/languages/supports/tokenization.ts#L412 (MIT License)
 	static function generateTokensCSSForColorMap (colorMap:Array<String>) : String
 	{
 		var rules = [];
@@ -24,7 +24,9 @@ class CSS
 
 		rules.push('.highlighted .mtki { font-style: italic; }');
 		rules.push('.highlighted .mtkb { font-weight: bold; }');
-		rules.push('.highlighted .mtku { text-decoration: underline; }');
+		rules.push('.highlighted .mtku { text-decoration: underline; text-underline-position: under; }');
+		rules.push('.highlighted .mtks { text-decoration: line-through; }');
+		rules.push('.highlighted .mtks.mtku { text-decoration: underline line-through; text-underline-position: under; }');
 
 		return rules.join('\n');
 	}
